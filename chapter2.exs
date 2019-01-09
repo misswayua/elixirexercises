736522e20ec260d43b0d723d525f84a6a73a5ada
Jackline Kaunda <wayua212@gmail.com>
2019-01-09 10:46:00 +0300

    done all chapter two exercises

--- chapter2.exs
+++ chapter2.exs
@@ -9,3 +9,24 @@ a = 4         => will match
 a = [[1,2,3]] => will match
 [a]= [[1,2,3]]=> will match
 [[a]]=[[1,2,3]] => will not match
+@doc """
+pattern matching-2
+which of the following will match
+"""
+[a,b,a] = [1,2,3]  => will not match
+[a,b,a] = [1,1,2]  => will not match
+[a,b,a] = [1,2,1]  => will not match
+
+@doc """
+pattern matching-3
+The variable a is bound to the value 2.
+which of the following will match
+"""
+[a,b,a] = [1,2,3]  => will not match
+[a,b,a] = [1,1,2]  => will not match
+ a = 1             => will match
+ ^a = 2            => will not match
+ ^a = 1            => will match    
+ ^a = 2-a          => will match
+
+
